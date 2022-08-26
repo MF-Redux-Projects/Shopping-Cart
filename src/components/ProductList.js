117305f47169641879e8ff63945f6e218ed2e3ca
mehedifoysal <mehedifoysal8@gmail.com>
2022-08-26 22:08:26 +0600

    components create

---
+++ src/components/ProductList.js
@@ -0,0 +1,9 @@
+const ProductList = () => {
+    return (
+        <div className="col-span-12 sm:col-span-12 md:col-span-7 lg:col-span-8 xxl:col-span-8">
+
+        </div>
+    );
+};
+
+export default ProductList;
